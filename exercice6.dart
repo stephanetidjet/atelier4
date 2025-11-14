b1b174297c7d531f430f4069d3d73aec641090f2
Stéphane <stephanetidjet2020@gmail.com>
2025-11-13 23:41:20 -0500

    2em  partie exo6

--- exercice6.dart
+++ exercice6.dart
@@ -2,3 +2,33 @@ abstract class Connectable {
   void connecter(String utilisateur);
   void deconnecter();
 }
+
+class ServeurAPI implements Connectable {
+  @override
+  void connecter(String utilisateur) {
+    print("ServeurAPI : Connexion établie pour $utilisateur.");
+  }
+
+  @override
+  void deconnecter() {
+    print("ServeurAPI : Déconnexion réussie.");
+  }
+}
+
+class BaseDeDonnees implements Connectable {
+  @override
+  void connecter(String utilisateur) {
+    print("BaseDeDonnees : Connexion ouverte pour $utilisateur.");
+  }
+
+  @override
+  void deconnecter() {
+    print("BaseDeDonnees : Déconnexion effectuée.");
+  }
+}
+
+
+
+
+
+
