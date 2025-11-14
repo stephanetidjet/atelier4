67c6cc1865b43ab8606d94bbf29271114960b44c
Stéphane <stephanetidjet2020@gmail.com>
2025-11-13 21:05:12 -0500

    2em partie exo1

--- exercice1.dart
+++ exercice1.dart
@@ -1,6 +1,25 @@
-class couleur {
+class Couleur {
   final int r;
   final int g;
   final int b;
-  couleur(this.r,this.g,this.b);
+  Couleur(this.r,this.g,this.b);
+  
+  Couleur.rouge()
+      : r = 255,
+        g = 0,
+        b = 0;
+
+  Couleur.vert()
+      : r = 0,
+        g = 255,
+        b = 0;
+
+  Couleur.bleu()
+      : r = 0,
+        g = 0,
+        b = 255;
+
+  void afficherCouleur() {
+    print("(R : $r, V : $g, B : $b)");
+  }
 }
