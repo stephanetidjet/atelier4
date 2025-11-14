6a4b1ae09c78a3a4d880de4648345aa0076b9412
Stéphane <stephanetidjet2020@gmail.com>
2025-11-14 00:11:16 -0500

    2em   partie exo8

--- exercice8.dart
+++ exercice8.dart
@@ -1,17 +1,26 @@
 class Livre {
   String titre;
   String auteur;
-   Livre(this.titre, this.auteur);
+   int _pages = 200;
+  static int totalLivres = 0;
 
+   Livre(this.titre, this.auteur) {
+    totalLivres++;
+   }
+
+   int get pages => _pages;
   void afficherInfos() {
     print("Titre: $titre, Auteur: $auteur");
+  }
+  static void afficherTotal() {
+    print("Total de livres créés : $totalLivres");
   }
 }
 
 void main() {
    Livre l1 = Livre("Les Misérables", "Victor Hugo");
-  Livre l2 = Livre("Le Petit Prince", "Antoine de Saint-Exupéry");
-  Livre l3 = Livre("L'Étranger", "Albert Camus");
+   Livre l2 = Livre("Le Petit Prince", "Antoine de Saint-Exupéry");
+   Livre l3 = Livre("L'Étranger", "Albert Camus");
 
   l1.afficherInfos();
   l2.afficherInfos();
