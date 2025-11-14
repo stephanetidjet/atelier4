f9b5b32ce06631dcbaa82626986d0da5de5267b8
Stéphane <stephanetidjet2020@gmail.com>
2025-11-13 21:18:31 -0500

    2eme  exo2

--- exercice2.dart
+++ exercice2.dart
@@ -3,4 +3,12 @@ class Point {
   double _y;
 
   Point(this._x, this._y);
+   void stephane(double dx, double dy) {
+    _x += dx;
+    _y += dy;
+  }
+
+  void display() {
+    print("Point(x: $_x, y: $_y)");
+  }
 }
