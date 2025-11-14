17ccb4abdfc67392f85f701e8ba933f0862dd8bc
Stéphane <stephanetidjet2020@gmail.com>
2025-11-13 22:14:49 -0500

    dernier   partie exo 5

--- exercice5.dart
+++ exercice5.dart
@@ -18,3 +18,26 @@ class Cercle extends Forme {
     return pi * rayon * rayon;
   }
 }
+
+class Rectangle extends Forme {
+  final double longueur;
+  final double largeur;
+
+  Rectangle(this.longueur, this.largeur);
+
+  @override
+  double calculerAire() {
+    return longueur * largeur;
+  }
+}
+
+void main() {
+  Cercle c = Cercle(3.0);
+  Rectangle r = Rectangle(4.0, 5.0);
+
+  c.afficherMessage();
+  print("Aire du cercle : ${c.calculerAire()}");
+
+  r.afficherMessage();
+  print("Aire du rectangle : ${r.calculerAire()}");
+}
