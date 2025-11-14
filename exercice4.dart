a500127731cd6a3b1aeba68204782c4b7b989fde
Stéphane <stephanetidjet2020@gmail.com>
2025-11-13 21:45:59 -0500

    2em  part 4

--- exercice4.dart
+++ exercice4.dart
@@ -7,3 +7,25 @@ class Media {
     print("Ceci est un média générique.");
   } 
 }
+
+class Livre extends  Media {
+  final String auteur;
+
+  Livre(String titre, this.auteur) : super(titre);
+
+  @override
+  void afficherType() {
+    print("Ceci est un Livre : $titre par $auteur.");
+  }
+}
+
+class Film extends Media {
+  final int dureeMinutes;
+
+  Film(String titre, this.dureeMinutes) : super(titre);
+
+  @override
+  void afficherType() {
+    print("Ceci est un Film : $titre, durée : $dureeMinutes minutes.");
+  }
+}
